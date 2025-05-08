686d70c06629ec278aa54562dbd80fa16bc17b2b
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-08 14:17:17 +0530

    terraform vm file

--- tf-state-ini/main.tf
+++ tf-state-ini/main.tf
@@ -1,20 +1,27 @@
-# provider "azurerm" {
-#   features {}
-# }
-#
-# resource "azurerm_resource_group" "golive" {
-#   name     = "golive-resources"
-#   location = "UK West"
-# }
-#
-# resource "azurerm_storage_account" "golive" {
-#   name                     = "golivestorage123" # must be globally unique & lowercase
-#   resource_group_name      = azurerm_resource_group.golive.name
-#   location                 = azurerm_resource_group.golive.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#
-#   tags = {
-#     environment = "demo"
-#   }
-# }
+provider "azurerm" {
+  features {}
+  subscription_id = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
+}
+
+resource "azurerm_storage_account" "golive" {
+  name = "golive.storage123" # must be globally unique & lowercase
+  resource_group_name      = "golive"
+  location                 = "UK west"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "golive.container"
+  storage_account_id    = azurerm_storage_account.golive.id
+  container_access_type = "private"
+}
+
+terraform {
+  backend "azurerm" {
+    #tenant_id                  = "00000000-0000-0000-0000-000000000000"
+    subscription_id             = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
+    resource_group_name         = "golive"
+    storage_account_name        = "golive.storage123"
+    container_name              = "golive.container"                               #
+}
