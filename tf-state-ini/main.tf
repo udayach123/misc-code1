f064d1b1282eed9d8bc34263d8a57fc51a76ec1f
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-09 20:05:04 +0530

    terraform vm file

--- tf-state-ini/main.tf
+++ tf-state-ini/main.tf
@@ -3,23 +3,33 @@ provider "azurerm" {
   subscription_id = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
 }
 
-resource "azurerm_storage_account" "golive" {
-  name = "goliveroboshop" # must be globally unique & lowercase
-  resource_group_name      = "golive"
-  location                 = "UK West"
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "golive"
+  location = "UK West"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = "storageroboshop"  # ✅ Removed the dot (invalid character)
+  resource_group_name      = azurerm_resource_group.rg.name  # ✅ Use .name, not .id
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "golivecontainer"
-  storage_account_id    = azurerm_storage_account.golive.id
+# Create a blob container
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate.container"
+  storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
 
+# Terraform backend configuration (must be in a separate file for initialization)
+# NOTE: This block should go in a separate file like backend.tf *before* you run `terraform init`
+
 terraform {
   backend "azurerm" {
-    #tenant_id                  = "00000000-0000-0000-0000-000000000000"
     subscription_id      = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
     resource_group_name  = "golive"
     storage_account_name = "goliveroboshop"
