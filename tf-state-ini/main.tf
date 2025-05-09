0a9f9f51ac82e8364723c0708ac332dc18267a66
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-09 20:06:02 +0530

    terraform vm file

--- tf-state-ini/main.tf
+++ tf-state-ini/main.tf
@@ -32,8 +32,8 @@ terraform {
   backend "azurerm" {
     subscription_id      = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
     resource_group_name  = "golive"
-    storage_account_name = "goliveroboshop"
-    container_name       = "golivecontainer"
+    storage_account_name = "storageroboshop"
+    container_name       = "tfstate.container"
     key                  = "prod.terraform.tfstate"
   }
 }
