4314edca7bdd6665ae8a84c6ae2a3a9d48690f53
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-14 13:28:23 +0530

    terraform vm file

--- vault-secrets/main.tf
+++ vault-secrets/main.tf
@@ -1,8 +1,14 @@
 resource "vault_mount" "kv" {
-  for_each =  var.secrets
+  for_each =  var.secret
   path        = each.key
   type        = "kv"
   options     = { version = "2" }
   description = each.value["description"]
 }
 
+resource "vault_kv_secret_v2" "values" {
+  for_each                   = var.values
+  mount                      = each.value["secret"]
+  name                       = each.key
+  data_json                  = jsonencode(each.value["value"])
+}
--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -1,9 +1,28 @@
 variable "token" {}
 
-variable "secrets" {
+variable "secret" {
   default = {
     roboshop-dev = {
       description = "Roboshop app component for all secrets"
+    }
+    roboshop-infra = {
+      description = "Roboshop infra related secrets"
+    }
+  }
+}
+
+variable "values" {
+  default = {
+    cart = {
+      secret = "roboshop-dev"
+    }
+
+    ssh = {
+      secret = "roboshop-infra"
+      value = {
+        username = "devops18"
+        password = "Passw0rd@1234"
+      }
     }
   }
 }
