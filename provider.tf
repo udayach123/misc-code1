a615a9b7d534a7ce57f78ad367a85295a6e47abb
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-15 05:53:19 +0530

    terraform vm file

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
-}
-
 provider "vault" {
   address = "http://vault.yourtherapist.in:8200"
   token   =  var.token
