50852b4aaf5bab3599cec9a83e8b6c395dcc2d33
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-24 19:24:32 +0530

    commit this

--- main.tf
+++ main.tf
@@ -4,4 +4,5 @@ module "tools-vm" {
   rg_location = var.rg_location
   rg_name     = var.rg_name
   name        = each.key
-  vm_size     = each.value[ "vm_size"
+  vm_size     = each.value[ "vm_size" ]
+}
